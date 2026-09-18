d5c666546f6b1342c5bbe92faf45468366dd1f99
marselF <fazleev_01@mail.ru>
2026-09-19 00:15:13 +0300

    delete py files

--- fix3.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('/Users/marselfazleev/dev/CSharp/Projects/ContainerRfBot/ContainerRfBot.Bot/Services/MaxBot/MaxBotService.cs', 'r') as f:
-    text = f.read()
-
-text = text.replace("chatId: chatId", "chatId: user.Id")
-
-with open('/Users/marselfazleev/dev/CSharp/Projects/ContainerRfBot/ContainerRfBot.Bot/Services/MaxBot/MaxBotService.cs', 'w') as f:
-    f.write(text)
--- fix_yk.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('/Users/marselfazleev/dev/CSharp/Projects/ContainerRfBot/ContainerRfBot.API/Controllers/YooKassaController.cs', 'r') as f:
-    text = f.read()
-
-text = text.replace("SendMessageAsync(userId", "SendMessageToUserAsync(userId")
-
-with open('/Users/marselfazleev/dev/CSharp/Projects/ContainerRfBot/ContainerRfBot.API/Controllers/YooKassaController.cs', 'w') as f:
-    f.write(text)
